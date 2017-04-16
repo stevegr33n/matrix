eab0bfa648f9ece365603a9372cafc209669d8b5
steve <bashcandicoot@gmail.com>
2017-04-16 19:12:31 +0100

    scrapped that other thing

--- matrix.js
+++ matrix.js
@@ -1,9 +1,7 @@
 'use strict'
 const SYMBOLS = '$,\xA5,\u2665'.split(',')
 const FONTSIZE = 30
-const BUFFER = FONTSIZE/3
-var y = FONTSIZE
-var x = BUFFER
+var drops = []
 
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
@@ -18,6 +16,7 @@ function updateCanvas () {
   setClearInterval()
   getCanvasSize()
   setFont()
+  setSymbolPositions()
   setDrawInterval()
 }
 function setClearInterval () {
@@ -31,40 +30,46 @@ function setFont() {
   ctx.font = FONTSIZE + "px Courier New"
 }
 function setDrawInterval () {
-  drawInterval = setInterval(draw, 515)
+  drawInterval = setInterval(draw, 115)
 }
 function getRandomChar () {
   return SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
 }
-function getRandomHexCode() {
-    var chars = '9ABCDEF';
-    var hexCode = '#';
-    for (var i = 0; i < chars.length; i++) {
-        hexCode += chars[Math.floor(Math.random() * chars.length)];
-    }
-    return color;
+function numOfColumns () {
+  return Math.round(canvas.width / FONTSIZE)
 }
+function setSymbolPositions() {
+  for (var i = 0; i < numOfColumns(); i++) {
+    drops[i] = 1
+  }
+  // console.log('(((',drops)
+  console.log('(((',drops)
+  console.log(numOfColumns())
+  console.log(canvas.width)
 
-// var drops = [];
-// for (var x = 0; x < canvas.width; x++) {
-//     drops[x] = 1;
-// }
-
+}
+function getRandomHexCode() {
+    var chars = '9ABCDEF'
+    var hexCode = '#'
+    for (var i = 0; i < chars.length; i++) {
+        hexCode += chars[Math.floor(Math.random() * chars.length)]
+    }
+    return color
+}
 function draw() {
-    // ctx.fillStyle = getRandomColor();
-    ctx.fillStyle = '#999999'
-    while (x < (canvas.width - BUFFER)) {
-      var char = getRandomChar()
-      ctx.fillText(char, x, y)
-      x += FONTSIZE
-    }
-    x = BUFFER
-    if (y < (canvas.height - BUFFER)) {
-      y += FONTSIZE
-    } else {
-      y = FONTSIZE
-    }
-    ctx.fillStyle = "rgba(0,0,0,.1)";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = getRandomColor();
+  ctx.fillStyle = '#999999'
+  for (var i = 0; i < drops.length; i++)  {
+    var symbol = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
 
+    ctx.fillText(symbol, FONTSIZE * i, drops[i] * FONTSIZE)
+
+    if (drops[i] * FONTSIZE > canvas.height && Math.random() > 0.95) {
+      console.log(drops[i] * FONTSIZE)
+      drops[i] = 0;
+    }
+    drops[i]+=1
+}
+ctx.fillStyle = "rgba(0,0,0,.1)"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
