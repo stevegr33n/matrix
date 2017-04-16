ca5132f37c61e1bf86598b8c35cc717534a87215
steve <bashcandicoot@gmail.com>
2017-04-16 17:13:10 +0100

    everything into functions, broke the randomness for a bit

--- matrix.js
+++ matrix.js
@@ -1,64 +1,70 @@
 'use strict'
-const CHARS = '$,\xA5,\u2665'.split(',')
+const SYMBOLS = '$,\xA5,\u2665'.split(',')
 const FONTSIZE = 30
+const BUFFER = FONTSIZE/3
+var y = FONTSIZE
+var x = BUFFER
 
 var canvas = document.getElementById("canvas")
 var ctx = canvas.getContext("2d")
-var columns = ((canvas.width / FONTSIZE) * FONTSIZE)
 var drawInterval = 0
 
-document.onload = updateCanvas()
-window.addEventListener('resize', updateCanvas, false)
-
-
+;(function init() {
+  document.onload = updateCanvas()
+  window.addEventListener('resize', updateCanvas, false)
+})()
 
 function updateCanvas () {
   setClearInterval()
-  updateCanvasSize()
+  getCanvasSize()
+  setFont()
   setDrawInterval()
 }
 function setClearInterval () {
   clearInterval(drawInterval)
 }
-function setDrawInterval () {
-  drawInterval = setInterval(draw, 120)
-}
-function updateCanvasSize () {
+function getCanvasSize () {
   canvas.height = window.innerHeight
   canvas.width = window.innerWidth
 }
-
-var drops = [];
-for (var x = 0; x < columns; x++) {
-    drops[x] = 1;
+function setFont() {
+  ctx.font = FONTSIZE + "px Courier New"
+}
+function setDrawInterval () {
+  drawInterval = setInterval(draw, 515)
+}
+function getRandomChar () {
+  return SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
+}
+function getRandomHexCode() {
+    var chars = '9ABCDEF';
+    var hexCode = '#';
+    for (var i = 0; i < chars.length; i++) {
+        hexCode += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return color;
 }
 
+// var drops = [];
+// for (var x = 0; x < canvas.width; x++) {
+//     drops[x] = 1;
+// }
+
 function draw() {
-
-    // function getRandomColor() {
-    //     var letters = '9ABCDEF';
-    //     var color = '#';
-    //     for (var i = 0; i < 6; i++) {
-    //         color += letters[Math.floor(Math.random() * 7)];
-    //     }
-    //     return color;
-    // }
     // ctx.fillStyle = getRandomColor();
-    ctx.fillStyle = '#999999';
-    ctx.font = FONTSIZE + "px Courier New";
-
-    for (var i = 0; i < drops.length; i++) {
-        var text = CHARS[Math.floor(Math.random() * CHARS.length)];
-        //console.log(text, FONTSIZE * i, drops[i] * FONTSIZE)
-        ctx.fillText(text, FONTSIZE * i, drops[i] * FONTSIZE);
-
-        if (drops[i] * FONTSIZE > canvas.height && Math.random() > 0.95) {
-            drops[i] = 0;
-        }
-        drops[i] += 1.1;
+    ctx.fillStyle = '#999999'
+    while (x < (canvas.width - BUFFER)) {
+      var char = getRandomChar()
+      ctx.fillText(char, x, y)
+      x += FONTSIZE
+    }
+    x = BUFFER
+    if (y < (canvas.height - BUFFER)) {
+      y += FONTSIZE
+    } else {
+      y = FONTSIZE
     }
     ctx.fillStyle = "rgba(0,0,0,.1)";
-
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 }
