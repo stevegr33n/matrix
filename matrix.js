d69b731e52860c166f2b6522528153f50f649e68
bashCandicoot <ste7@protonmail.com>
2018-09-02 20:00:27 +0100

    spruced up

--- matrix.js
+++ matrix.js
@@ -1,103 +1,19 @@
-'use strict'
-const SYMBOLS = '$,\xA5,\u2665'.split(',')
-const FONTSIZE = 40
-var yPositions = []
+import MatrixService from './matrixService'
 
-const CANVAS = document.getElementById("canvas")
-const CTX = CANVAS.getContext("2d")
-const DRAWSPEED = 115
-var drawInterval = 0
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const matrix = new MatrixService({canvas, ctx})
+window.addEventListener('resize', updateCanvas, false);
 
-;(function init() {
-  document.onload = updateCanvas()
-  window.addEventListener('resize', updateCanvas, false)
-})()
-
-function updateCanvas () {
-  setClearInterval()
-  getCanvasSize()
-  setFont()
-  setRandomSymbolStartYPositions()
-  setDrawInterval()
+function updateCanvas() {
+  matrix.getCanvasSize()
+  matrix.setFont()
+  matrix.setRandomSymbolStartYPositions()
 }
 
-function setClearInterval () {
-  clearInterval(drawInterval)
-}
+function initCanvas() {
+  updateCanvas()
+  matrix.drawSymbolsInterval()
+};
 
-function getCanvasSize () {
-  CANVAS.height = window.innerHeight
-  CANVAS.width = window.innerWidth
-}
-
-function setFont() {
-  CTX.font = FONTSIZE + "px Courier New"
-}
-
-function setRandomSymbolStartYPositions() {
-  yPositions = [] // empty the previous positions whenever window resizes
-  for (var i = 0; i < getNumOfColumns(); i++) {
-    yPositions[i] = randomInt(FONTSIZE)
-  }
-}
-
-function setDrawInterval () {
-  drawInterval = setInterval(drawSymbols, DRAWSPEED)
-}
-
-function getNumOfColumns () {
-  return Math.round(CANVAS.width / FONTSIZE)
-}
-
-function getRandomChar () {
-  return SYMBOLS[randomInt(SYMBOLS.length)]
-}
-
-function randomInt(max) {
-  return Math.floor(Math.random() * max)
-}
-
-function randomBool() {
-  return Math.random() > 0.95 ? true : false
-}
-//
-// function getRandomHexCode() {
-//     var chars = 'AB999F'
-//     var hexCode = '#'
-//     for (var i = 0; i < chars.length; i++) {
-//         hexCode += chars[randomInt(chars.length)]
-//     }
-//     console.log(hexCode)
-//     return hexCode
-// }
-// function getRandomRedHexCode() {
-//     var chars = '993359'
-//     var hexCode = '#'
-//     for (var i = 0; i < chars.length; i++) {
-//         hexCode += chars[randomInt(chars.length)]
-//     }
-//     console.log(hexCode)
-//     return hexCode
-// }
-
-function drawSymbols() {
-  CTX.fillStyle = '#993359'
-  // CTX.fillStyle = getRandomHexCode();
-  var yPos
-  var xPos
-  var symbol
-  for (var i = 0; i < yPositions.length; i++)  {
-    yPos = yPositions[i] * FONTSIZE
-    xPos = FONTSIZE * i
-    symbol = SYMBOLS[randomInt(SYMBOLS.length)]
-    CTX.fillText(symbol, xPos, yPos)
-
-    if (yPositions[i] * FONTSIZE > CANVAS.height && randomBool()) {
-      // If yPos is more than the canvas, and some random event is true
-      yPositions[i] = 0;
-    }
-    yPositions[i] += 1
-  }
-  CTX.fillStyle = "rgba(0,0,0,.1)"
-  CTX.fillRect(0, 0, CANVAS.width, CANVAS.height)
-}
+initCanvas()
---
+++ matrixService.js
@@ -0,0 +1,89 @@
+export default class MatrixService {
+	constructor({canvas, ctx}) {
+		this.canvas = canvas
+		this.ctx = ctx
+		this.drawInterval = 115
+		this.fontSize = 40
+		this.symbols = '$,\xA5,\u2665'.split(',')
+		this.yPositions = []
+	}
+
+	getCanvasSize () {
+		this.canvas.height = window.innerHeight
+		this.canvas.width = window.innerWidth
+	}
+	
+	drawSymbolsInterval () {
+			setInterval (() => this.drawSymbols(), this.drawInterval);
+	}
+	
+	getNumOfColumns () {
+		return Math.round(this.canvas.width / this.fontSize)
+	}
+
+	setFont() {
+		this.ctx.font = this.fontSize + 'px Courier New'
+	}
+	
+	getRandomChar () {
+		return this.symbols[randomInt(this.symbols.length)]
+	}
+	
+	randomInt(max) {
+		return Math.floor(Math.random() * max)
+	}
+	
+	randomBool() {
+		return Math.random() > 0.95 ? true : false
+	}
+	
+	isYPosGreaterThanCanvasHeight(yPos) {
+		return yPos * this.fontSize > this.canvas.height
+	}
+	
+	fillText({ symbol, xPos, yPos }) {
+		this.ctx.fillText(symbol, xPos, yPos);
+	}
+
+	setRandomSymbolStartYPositions() {
+		this.yPositions = []
+		for (let i = 0; i < this.getNumOfColumns(); i++) {
+			this.yPositions[i] = this.randomInt(this.fontSize)
+		}
+	}
+
+	drawSymbols() {
+		this.ctx.fillStyle = '#993359'
+		// ctx.fillStyle = getRandomHexCode();
+		console.log(this.drawInterval)
+		this.yPositions.forEach((yPos, index, yPositions) => {
+			this.fillText({
+				symbol: this.symbols[this.randomInt(this.symbols.length)],
+				xPos: this.fontSize * index,
+				yPos: yPos * this.fontSize
+			});
+			this.isYPosGreaterThanCanvasHeight && this.randomBool() ?  yPositions[index] = 0 : yPositions[index] += 1;
+		})
+		this.ctx.fillStyle = 'rgba(0,0,0,0.2)';
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
+// function getRandomHexCode() {
+//     let chars = 'AB999F'
+//     let hexCode = '#'
+//     for (let i = 0; i < chars.length; i++) {
+//         hexCode += chars[randomInt(chars.length)]
+//     }
+//     console.log(hexCode)
+//     return hexCode
+// }
+// function getRandomRedHexCode() {
+//     let chars = '993359'
+//     let hexCode = '#'
+//     for (let i = 0; i < chars.length; i++) {
+//         hexCode += chars[randomInt(chars.length)]
+//     }
+//     console.log(hexCode)
+//     return hexCode
+// }
